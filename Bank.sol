105f3cc4b3fa182e267a774d370962d0c9393bd6
imminer668 <imminer@163.com>
2024-01-28 18:43:58 +0800

    Add event

--- Bank.sol
+++ Bank.sol
@@ -1,37 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-contract Bank{
 
-//setAdmin
-address  admin;
-//balances User address => amount
-mapping(address => uint) public userBalances;
+contract Bank {
+    //setAdmin
+    address admin;
+    //balances User address => amount
+    mapping(address => uint) public userBalances;
 
-//top3 
-address[3] public top3Address;
+    //top3
+    address[3] public top3Address;
 
-//totalBalances
-uint256 public  totalBalances=0;
-constructor() {
-    admin=msg.sender;
-}
+    //totalBalances
+    uint256 public totalBalances = 0;
 
+    event Deposit(address user,uint256 value,uint256 userBalances,uint256 totalBalances);
+    event Top3Is(address user1,address user2,address user3);
+    event WithdrawWithAdmin(address user,uint256 value);
 
-function deposit() public  payable{
-    require(msg.value>0,"eth must be greater than 0");
-    uint256 data =userBalances[msg.sender]+msg.value;
-    userBalances[msg.sender]=data;
-    totalBalances=totalBalances+msg.value;
-    sort(msg.sender,data);
+    constructor() {
+        admin = msg.sender;
+    }
 
+    function deposit() public payable {
+        require(msg.value > 0, "eth must be greater than 0");
+        uint256 data = userBalances[msg.sender] + msg.value;
+        userBalances[msg.sender] = data;
+        totalBalances = totalBalances + msg.value;
+        sort(msg.sender, data);
+        emit Deposit(msg.sender,msg.value,userBalances,totalBalances);
+    }
 
-} 
-function sort(address s, uint256 data) internal {
-
+    function sort(address s, uint256 data) internal {
         uint256 min = data;
         uint8 index = 3;
         for (uint8 i = 0; i < 3; i++) {
-            //exixt 
+            //exixt
             if (top3Address[i] == s) {
                 index = 3;
                 break;
@@ -44,14 +47,17 @@ function sort(address s, uint256 data) internal {
         if (index < 3) {
             top3Address[index] = s;
         }
-}
-function getTop3() public view returns(address[3] memory){
-return top3Address;
-}
+        emit Top3Is(top3Address[0],top3Address[1],top3Address[2]);
+    }
 
-function withdraw()public {
-require(msg.sender==admin,"only admin can withdraw eth");
-payable(msg.sender).transfer(totalBalances);
-totalBalances=0;
-}
+    function getTop3() public view returns (address[3] memory) {
+        return top3Address;
+    }
+
+    function withdraw() public {
+        require(msg.sender == admin, "only admin can withdraw eth");
+        payable(msg.sender).transfer(totalBalances);
+        emit WithdrawWithAdmin(msg.sender,totalBalances);
+        totalBalances = 0;
+    }
 }
